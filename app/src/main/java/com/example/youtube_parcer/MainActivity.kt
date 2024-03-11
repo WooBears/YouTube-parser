9c4dfe5e7390e074e7abb738559bbabd7c2d6fd8
pahlavidavlatmirov <p.davlatmirov99@gmail.com>
2024-03-11 14:33:23 +0300

    YouTube Parcer (1st commit xml)

---
+++ app/src/main/java/com/example/youtube_parcer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.youtube_parcer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+    }
+}
